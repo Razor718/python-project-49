9da6b98aca3253f5e3c914bffe5ea9c5e4494d27
Дмитрий Горбанев <dima.gorbanev99@mail.ru>
2024-04-07 23:55:34 +0300

    calc is ready

--- games/calc.py
+++ games/calc.py
@@ -15,23 +15,36 @@ def welcome_user():
 main()
 name_ = welcome_user()
 print('What is the result of the expression?')
+n = 0
 
 
 def get_expression():
     a = random.randint(1, 1000)
     b = random.randint(1, 1000)
-    operators = ['+', '-', '*', '/']
+    operators = ['+', '-', '*']
     random_operator = random.choice(operators)
-    expression = a, random_operator, b
-    print(f'Question: {expression}')
-    answer = input()
-    print('Your answer: ' + answer)
-    if answer == :
-       print('Correct!')
+    print('Question:', a, random_operator, b)
+    answer = int(input())
+    print('Your answer: ', answer)
+    c = 0
+    if random_operator == '+':
+        c = a + b
+    if random_operator == '-':
+        c = a - b
+    if random_operator == '*':
+        c = a * b
+    if c == answer:
+        print('Correct!')
     else:
         print(f'''{answer} is wrong answer ;(.
-              Correct answer was .
-              Let`s try again, {name_}''') 
+            Correct answer was {c}.
+            Let`s try again, {name_}!''')
+        global n
+        n = 1
 
 
-get_expression()
+for i in range(3):
+    i += 1
+    get_expression()
+if n == 0:
+    print(f'Congratulations, {name_}!')
