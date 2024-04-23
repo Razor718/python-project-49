42f85ebf3cd0001eb6bd0a0d7a9f5b8413496ebe
Дмитрий Горбанев <dima.gorbanev99@mail.ru>
2024-04-23 15:47:16 +0300

    add game brain_progression

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,3 @@
+from games.progression import main
+if __name__ == '__main__':
+    main()
---
+++ games/progression.py
@@ -0,0 +1,43 @@
+import prompt
+import random
+def main():
+    print("Welcome to the Brain Games!")
+
+main()
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print('Hello,', name + '!')
+    return name
+
+def get_progression():
+    name_ = welcome_user()
+    print('What number is missing in the proggresion?')
+    userscore = 0
+    winscore = 3
+    # Циклом while ставлю ограничение на количество раундов
+    while userscore < winscore:
+        a = random.randint(1, 30)
+        b = random.randint(31, 50)
+        result = []
+        # Циклом for генерирую последовательность
+        for i in range(a, b, 3):
+            result.append(i)
+        random_index = result.pop(random.randint(0, len(result) - 1))
+        print(f'Question: {result}')
+        answer = prompt.integer('Your asnwer: ')
+        # Делаю проверку на правильность ответа
+        if answer == random_index:
+            print('Correct!')
+            userscore += 1
+        else:
+            print(
+        f'''Question: {result}
+        Your answer: {answer}
+        {answer} is wrong answer ;(. Correct answer was {random_index}
+        Let`s try again, {name_}''')
+            break
+        if userscore == 3:
+            print(f'Congratulations, {name_}')
+        
+get_progression()
