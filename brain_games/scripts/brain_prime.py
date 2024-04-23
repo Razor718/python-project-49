e5ad8b3e6e88b240442708358ae107c00ce7b261
Дмитрий Горбанев <dima.gorbanev99@mail.ru>
2024-04-23 18:34:50 +0300

    add game brain_prime

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,3 @@
+from games.prime import main
+if __name__ == '__main__':
+    main()
---
+++ games/prime.py
@@ -0,0 +1,50 @@
+import prompt
+import random
+def main():
+    print("Welcome to the Brain Games!")
+
+main()
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print('Hello,', name + '!')
+    return name
+
+def is_simple(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+def check_simplicity():
+    name_ = welcome_user()
+    print('Answer "yes" if given number is prime. Otherwise answer "no"')
+    userscore = 0
+    winscore = 3
+    while userscore < winscore:
+        random_number = random.randint(1, 100)
+        is_prime = is_simple(random_number)
+        print(f'Question: {random_number}')
+        answer = prompt.string('Your answer: ')
+        if is_prime and answer == 'yes':
+            print('Correct!')
+            userscore += 1
+        elif not is_prime and answer == 'no':
+            print('Correct!')
+            userscore += 1
+        else:
+            print(
+        f'''Question: {random_number}
+        Your answer: {answer}
+        {answer} is wrong asnwer;(.
+        Correct asnwer was {'yes' if is_prime else 'no'}
+        Let`s try again, {name_}''')
+            break
+        if userscore == 3:
+            print(f'Congratulation, {name_}')
+
+check_simplicity()
+        
