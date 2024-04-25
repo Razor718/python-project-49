c6ee3607cee2a398d3e8d24b3a396da1d231a2b7
Дмитрий Горбанев <dima.gorbanev99@mail.ru>
2024-04-25 20:23:44 +0300

    fix even.py

--- games/even.py
+++ games/even.py
@@ -15,34 +15,30 @@ def main():
 main()
 
 
-name_ = welcome_user()
-print('Answer "yes" if the number is even, otherwise answer "no".')
-n = 0
-
 
 def get_number():
-    random_number = randint(1, 1000)
-    print(f'Question: {random_number}')
-    answer = input()
-    print('Your answer: ' + answer)
-    if random_number % 2 == 0 and answer == 'yes':
-        print('Correct!')
-    elif random_number % 2 != 0 and answer == 'no':
-        print('Correct!')
-    else:
-        global n
-        n = 1
-        if answer == 'yes':
-            correct_answer = 'no'
+    name_ = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    usercore = 0
+    winscore = 3
+    while usercore < winscore:
+        random_number = randint(1, 1000)
+        print(f'Question: {random_number}')
+        answer = input()
+        print('Your answer: ' + answer)
+        if random_number % 2 == 0 and answer != 'yes':
+            print(f'''{answer} is wrong answer ;(.
+            Correct answer was yes.
+            Let`s try again, {name_}''')
+            break
+        elif random_number % 2 != 0 and answer != 'no':
+            print(f'''{answer} is wrong answer ;(.
+            Correct answer was no.
+            Let`s try again, {name_}''')
+            break
         else:
-            correct_answer = 'yes'
-        print(f'''{answer} is wrong answer ;(.
-              Correct answer was {correct_answer}.
-              Let`s try again, {name_}''')
-
-
-for i in range(3):
-    i += 1
-    get_number()
-if n == 0:
-    print(f'Congratulations, {name_}!')
+            print('Correct!')
+            usercore += 1
+        if usercore == 3:
+            print(f'Congratulation, {name_}!') 
+get_number()
